229f799ed6cfcb3b10a47b917f33f8f5fe10ce4c
nicolas-repiquet <nicolas-repiquet@github.com>
2013-12-18 15:25:32 +0100

    internal fonts

---
+++ Granite.UI/Glyph.cs
@@ -0,0 +1,22 @@
+﻿using Granite3D;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Granite.UI
+{
+    public sealed class Glyph
+    {
+        private readonly Vector2i m_size;
+        private readonly Texture2D m_texture;
+        private readonly Box2 m_textureCoordinates;
+
+        public Glyph(Vector2i size, Texture2D texture, Box2 textureCoordinates)
+        {
+            m_size = size;
+            m_texture = texture;
+            m_textureCoordinates = textureCoordinates;
+        }
+    }
+}
--- Granite.UI/InternalFonts.cs
+++ Granite.UI/InternalFonts.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Granite3D;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Granite.UI
 {
-    class InternalFonts
+    internal static partial class InternalFonts
     {
+        private static readonly Lazy<Texture2D> s_texture;
+
+        static InternalFonts()
+        {
+            s_texture = new Lazy<Texture2D>(() =>
+            {
+                return null;
+            });
+        }
     }
 }
