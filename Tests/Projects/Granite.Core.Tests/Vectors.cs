a29f79ff6fb9dd29c4d9d1f15d4b78b41feb2fc7
nicolas-repiquet <nicolas.repiquet@gmail.com>
2014-11-08 23:14:51 +0100

    project files overhaul

---
+++ Tests/Projects/Granite.Core.Tests/Vectors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Granite.Core.Tests
+{
+    [TestClass]
+    public class Vectors
+    {
+        [TestMethod]
+        public void Addition()
+        {
+            var v = Vector2.UnitX + Vector2.UnitY;
+
+            Assert.AreEqual(v.X, 1);
+            Assert.AreEqual(v.Y, 1);
+        }
+    }
+}
