67cdf47e43f8b71a6ad61ae734bbfc371592304e
Lysfith <julien.plateau@gmail.comgit>
2014-12-15 13:29:55 +0100

    [Zombie] Add DayTime for better transitions of day and night

--- Sandbox/Zombie/Zombie/Game/Entities/World/DayLight.cs
+++ Sandbox/Zombie/Zombie/Game/Entities/World/DayLight.cs
@@ -16,13 +16,19 @@ namespace Zombie.Game.Entities.World
         private Color4 m_maxLight;
         private Color4 m_maxDark;
         private Map m_map;
+        private TimeSpan m_time;
 
         private Framebuffer m_framebufferAllLight;
         private Texture2D m_textureLight;
 
+        private TimeSpan m_timeStartNight;
+        private TimeSpan m_timeStartDay;
+        private TimeSpan m_timeTransition;
+
         private bool m_isUpdated;
 
-        public double DayProgress { get { return m_currentTime * 100 / m_timeOfDay;  } }
+        public double DayProgress { get { return m_currentTime * 100 / m_timeOfDay; } }
+        public TimeSpan DayTime { get { return m_time; } }
 
         public DayLight(Map map, double timeOfDay)
         {
@@ -33,6 +39,10 @@ namespace Zombie.Game.Entities.World
             m_maxLight = new Color4(1, 1, 1, 1);
             //m_maxLight = new Color4(0.6f, 0.6f, 0.6f, 1);
             m_maxDark = new Color4(0.1f, 0.1f, 0.05f, 1);
+
+            m_timeStartDay = new TimeSpan(07, 0, 0);
+            m_timeStartNight = new TimeSpan(19, 0, 0);
+            m_timeTransition = new TimeSpan(2, 0, 0);
 
             //Framebuffers
             m_framebufferAllLight = new Framebuffer();
@@ -54,6 +64,15 @@ namespace Zombie.Game.Entities.World
             m_isUpdated = false;
         }
 
+        public void ConvertDayProgressToTime()
+        {
+            var secondsInDay = 24 * 3600;
+
+            var progress = (1-(DayProgress/100.0)) * secondsInDay;
+
+            m_time = TimeSpan.FromSeconds(progress);
+        }
+
         public void Update(TimeSpan elapsed)
         {
             m_currentTime -= elapsed.TotalSeconds;
@@ -70,6 +89,8 @@ namespace Zombie.Game.Entities.World
                 m_textureLight.SetData<Color4ub>(size.X, size.Y, null);
                 m_framebufferAllLight.Attach(m_textureLight);
             }
+
+            ConvertDayProgressToTime();
         }
 
         public void Render(Matrix4 transform)
@@ -77,32 +98,35 @@ namespace Zombie.Game.Entities.World
             GL.BindFramebuffer(m_framebufferAllLight);
 
             //Calcul du pourcentage de la journée passé
-            var percentOfDay = DayProgress;
-            if (percentOfDay < 40 && percentOfDay > 10)
+            if (m_time > m_timeStartNight && m_time <= m_timeStartNight + m_timeTransition)
             {
-                var percentOfNight = (percentOfDay - 10) / (40f - 10f);
+                var percentOfTransitionInDay = m_timeTransition.TotalSeconds;
+                var percentOfTransition = (m_time - m_timeStartNight).TotalSeconds;
+                var percentOfNight = percentOfTransition / percentOfTransitionInDay;
 
                 var color = new Color4(
-                    (float)((m_maxLight.R - m_maxDark.R) * percentOfNight + m_maxDark.R),
-                    (float)((m_maxLight.G - m_maxDark.G) * percentOfNight + m_maxDark.G),
-                    (float)((m_maxLight.B - m_maxDark.B) * percentOfNight + 0.2 + m_maxDark.B),
+                    (float)((m_maxLight.R - m_maxDark.R) * (1-percentOfNight) + m_maxDark.R),
+                    (float)((m_maxLight.G - m_maxDark.G) * (1 - percentOfNight) + m_maxDark.G),
+                    (float)((m_maxLight.B - m_maxDark.B) * (1 - percentOfNight) + 0.2 + m_maxDark.B),
                     1);
 
                 GL.ClearColor(color);
             }
-            else if (percentOfDay > 60 && percentOfDay < 90)
+            else if (m_time > m_timeStartDay && m_time <= m_timeStartDay + m_timeTransition)
             {
-                var percentOfNight = (90f - percentOfDay) / (90f - 60f);
+                var percentOfTransitionInDay = m_timeTransition.TotalSeconds;
+                var percentOfTransition = (m_time - m_timeStartDay).TotalSeconds;
+                var percentOfDay = percentOfTransition / percentOfTransitionInDay;
 
                 var color = new Color4(
-                    (float)((m_maxLight.R - m_maxDark.R) * percentOfNight + 0.3f + m_maxDark.R),
-                    (float)((m_maxLight.G - m_maxDark.G) * percentOfNight + 0.1f + m_maxDark.G),
-                    (float)((m_maxLight.B - m_maxDark.B) * percentOfNight + m_maxDark.B),
+                    (float)((m_maxLight.R - m_maxDark.R) * percentOfDay + 0.3f + m_maxDark.R),
+                    (float)((m_maxLight.G - m_maxDark.G) * percentOfDay + 0.1f + m_maxDark.G),
+                    (float)((m_maxLight.B - m_maxDark.B) * percentOfDay + m_maxDark.B),
                     1);
 
                 GL.ClearColor(color);
             }
-            else if (percentOfDay > 40 && percentOfDay < 60)
+            else if (m_time > m_timeStartDay + m_timeTransition && m_time <= m_timeStartNight)
             {
                 GL.ClearColor(m_maxLight);
             }
---
+++ Sandbox/Zombie/Zombie/Game/Textures/SnowTexture.cs
@@ -0,0 +1,40 @@
+﻿using Granite.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Zombie.Game.Entities.Zones;
+using Zombie.Game.Sprites;
+
+namespace Zombie.Game.Textures
+{
+    public class SnowTexture
+    {
+        private static SnowTexture s_instance;
+        public static SnowTexture Instance
+        {
+            get
+            {
+                if (s_instance == null)
+                {
+                    s_instance = new SnowTexture();
+                }
+                return s_instance;
+            }
+        }
+
+        private readonly Texture2D m_backgroundTexture;
+        private readonly SpriteSheet m_backgroundSheet;
+        private readonly Sprite m_backgroundSprite;
+
+        public SpriteSheet SpriteSheet { get { return m_backgroundSheet; } }
+        public Sprite Sprite { get { return m_backgroundSprite; } }
+
+        public SnowTexture()
+        {
+            m_backgroundTexture = Texture2D.Load(typeof(Floor).Assembly.GetManifestResourceStream("Zombie.Game.Textures.snow.jpg"));
+            m_backgroundSheet = new SpriteSheet(m_backgroundTexture);
+            m_backgroundSprite = m_backgroundSheet.AddSprite("", new Box2i(Vector2i.Zero, m_backgroundTexture.Size));
+        }
+    }
+}
